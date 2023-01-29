1eb9be16835647a54f73587625c38e9a918616df
Harvey Sanders <sanders.harvey@gmail.com>
2023-01-28 20:03:54 -0500

    Wire ping stats to message traffic

---
+++ src/store/ping.ts
@@ -0,0 +1,23 @@
+import { writable } from 'svelte/store';
+import { PingStats, RoundtripTimer } from '../lib/ping/PingStats';
+
+function createStats() {
+    const timer = new RoundtripTimer();
+    const { subscribe, set, update } = writable<PingStats>(new PingStats());
+
+    return {
+        subscribe,
+        reset() {
+            set(new PingStats());
+        },
+        msgOut(msgId: string) {
+            timer.startTimer(msgId);
+        },
+        msgIn(msgId: string) {
+            const roundtripTime = timer.stopTimer(msgId);
+            update((stats) => stats.calcStats(roundtripTime));
+        },
+    };
+}
+
+export const pingStats = createStats();
