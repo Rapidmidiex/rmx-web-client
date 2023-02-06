a529784da2180836e0a8ff128de707cc9836c61a
Harvey Sanders <sanders.harvey@gmail.com>
2023-02-06 10:51:17 -0500

    Try soundfont-player

--- src/lib/components/jam/midi.ts
+++ src/lib/components/jam/midi.ts
@@ -1,12 +1,27 @@
-import * as JZZ from 'jzz';
+import * as SoundFont from 'soundfont-player';
 import { Envelope } from '@lib/envelope/envelope';
 import { NoteState, type MIDIMsg } from '@lib/types/jam';
 import { WSMsgTyp, type WSMsg } from '@lib/types/websocket';
 
-import * as JZZSynthTiny from 'jzz-synth-tiny';
-JZZSynthTiny(JZZ);
-// @ts-ignore
-const synth = JZZ.synth.Tiny() as typeof JZZ.MIDI;
+class Instrument {
+    private ac: AudioContext;
+    private player: SoundFont.Player;
+    constructor() {
+        this.ac = new AudioContext();
+        SoundFont.instrument(this.ac, 'acoustic_grand_piano')
+            .then((i) => {
+                this.player = i;
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+
+    noteOn(note: number, velocity: number) {
+        this.player.play(note);
+    }
+}
+
 /**
  *
  * @param wsClient WebSocket Client
@@ -21,16 +36,18 @@ export function noteHandler(wsClient: WebSocket, userId: string) {
     };
 }
 
+const instrument = new Instrument();
+
 export function handleIncomingMIDI(msg: WSMsg<MIDIMsg>) {
     // TODO play different users on different channels
     // TODO: use instrument from MIDI message
     const midi = msg.payload;
     switch (midi.state) {
         case NoteState.NOTE_ON:
-            synth.noteOn(0, midi.number, midi.velocity);
+            instrument.noteOn(midi.number, midi.velocity);
             break;
         case NoteState.NOTE_OFF:
-            synth.noteOff(0, midi.number);
+            // synth.noteOff(0, midi.number);
             break;
 
         default:
