c1388eb67456277263305762217d91ef732037b7
Harvey Sanders <sanders.harvey@gmail.com>
2023-02-06 06:27:27 -0500

    Documentation

--- src/lib/components/jam/mic.ts
+++ src/lib/components/jam/mic.ts
@@ -1,62 +1,101 @@
+/**
+ *
+ * @param buf Array of audio sample values, represented as floats ranging from -1 to 1.
+ * @param sampleRate Number of samples per second. Ex: (44100, 48000).
+ * @param threshold Minimum loudness level needed to attempt auto note correlation. Sounds with RMS levels below the threshold are discarded.
+ * @returns a float representing the estimated frequency (hz) of average loudest sound contained in the buffer.
+ */
 export function autoCorrelate(
     buf: Float32Array,
     sampleRate: number,
-    sensitivity: number
+    threshold: number
 ) {
     let SIZE = buf.length;
-    let rms = 0;
-    for (let i = 0; i < SIZE; i++) {
-        const val = buf[i];
-        rms += val * val;
-    }
-    rms = Math.sqrt(rms / SIZE);
-    console.log(Math.round(rms * 100));
-    if (rms < sensitivity) return -1;
-    let r1 = 0,
-        r2 = SIZE - 1,
-        thres = 0.2;
+    const rms = calcRMS(buf);
+
+    // Stop if RMS below threshold
+    if (rms < threshold) return -1;
+
+    // Trim buffer to the section containing levels above the thres.
+    let leftBoundary = 0;
+    let rightBoundary = SIZE - 1;
+    let thres = 0.2;
+    // Trim left
     for (let i = 0; i < SIZE / 2; i++)
         if (Math.abs(buf[i]) < thres) {
-            r1 = i;
+            leftBoundary = i;
             break;
         }
+    // Trim right
     for (let i = 1; i < SIZE / 2; i++)
         if (Math.abs(buf[SIZE - i]) < thres) {
-            r2 = SIZE - i;
+            rightBoundary = SIZE - i;
             break;
         }
-    buf = buf.slice(r1, r2);
+    buf = buf.slice(leftBoundary, rightBoundary);
     SIZE = buf.length;
-    const c = new Array(SIZE).fill(0);
-    for (let i = 0; i < SIZE; i++)
-        for (let j = 0; j < SIZE - i; j++) c[i] = c[i] + buf[j] * buf[j + i];
-    let d = 0;
-    while (c[d] > c[d + 1]) d++;
-    let maxval = -1,
-        maxpos = -1;
-    for (let i = d; i < SIZE; i++) {
-        if (c[i] > maxval) {
-            maxval = c[i];
-            maxpos = i;
+    // End Trim
+
+    // Create a new buffer with the trimmed size.
+    let cBuf = new Array(SIZE).fill(0);
+    for (let i = 0; i < SIZE; i++) {
+        for (let j = 0; j < SIZE - i; j++) {
+            // TODO: Document this
+            cBuf[i] = cBuf[i] + buf[j] * buf[j + i];
         }
     }
-    let T0 = maxpos;
-    const x1 = c[T0 - 1],
-        x2 = c[T0],
-        x3 = c[T0 + 1];
-    const a = (x1 + x3 - 2 * x2) / 2,
-        b = (x3 - x1) / 2;
-    if (a) {
-        T0 = T0 - b / (2 * a);
+
+    let d = 0;
+    while (cBuf[d] > cBuf[d + 1]) d++;
+    let maxVal = -1;
+    let maxPos = -1;
+    for (let i = d; i < SIZE; i++) {
+        if (cBuf[i] > maxVal) {
+            maxVal = cBuf[i];
+            maxPos = i;
+        }
     }
+    let T0 = maxPos;
+    let x1 = cBuf[T0 - 1];
+    let x2 = cBuf[T0];
+    let x3 = cBuf[T0 + 1];
+    let a = (x1 + x3 - 2 * x2) / 2;
+    let b = (x3 - x1) / 2;
+    if (a) T0 = T0 - b / (2 * a);
     return sampleRate / T0;
 }
 
-export function noteFromPitch(frequency: number, octaveLength: number) {
+/**
+ * Calculates the root-mean-squared (RMS) of audio samples. The RMS is the average squared value of all the samples in a buffer.
+ */
+function calcRMS(buf: Float32Array): number {
+    let rms = 0;
+    const size = buf.length;
+    for (let i = 0; i < size; i++) {
+        let val = buf[i];
+        rms += val * val;
+    }
+    return Math.sqrt(rms / size);
+}
+
+/**
+ * Converts audio frequency (hz) to MIDI note number.
+ * @example
+ * noteFromPitch(440) === 60
+ * @param frequency hertz
+ * @param octaveLength Number of notes in an octave. Defaults to 12.
+ * @returns MIDI note number
+ */
+export function noteFromPitch(frequency: number, octaveLength: number = 12) {
     var noteNum = octaveLength * (Math.log(frequency / 440) / Math.log(2));
     return Math.round(noteNum) + 69;
 }
 
+/**
+ * Calculates the average decibel level across all frequencies.
+ * @param analyser AnalyserNode
+ * @returns An integer representing decibel level.
+ */
 export function meter(analyser: AnalyserNode) {
     const array = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(array);
