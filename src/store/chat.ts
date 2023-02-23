41fefb506b89c4f5e3754684b876a1422f82c790
aboublef <kristopherab@gmail.com>
2023-02-23 23:31:38 +0000

    looking at a more advance svelte store api design

--- src/store/chat.ts
+++ src/store/chat.ts
@@ -1,6 +1,40 @@
+import type { Message, MessagePayload } from "@lib/envelope/message";
 import { storage } from '@lib/services/store/store';
-import type { TextMsg } from '@lib/types/jam';
-import type { WSMsg } from '@lib/types/websocket';
+import { get, writable } from "svelte/store";
 
 // TODO -- omitting `type` as not really needed here
-export const ChatStore = storage<Omit<WSMsg<TextMsg>, "type">[]>('CHAT_STORE', []);
+export const ChatStore = storage<Omit<Message<"text">, "type">[]>('CHAT_STORE', []);
+
+
+type Chat = MessagePayload["text"]
+
+const ChatStorage = (storageName = "", initValue = []) =>{
+    const store = writable(initValue);
+
+    const storedValueStr = localStorage.getItem(storageName);
+    if (storedValueStr != null) store.set(JSON.parse(storedValueStr));
+
+    store.subscribe((val) => {
+        if ([null, undefined].includes(val)) {
+            localStorage.removeItem(storageName);
+        } else {
+            localStorage.setItem(storageName, JSON.stringify(val));
+        }
+    });
+
+    window.addEventListener('storage', () => {
+        const storedValueStr = localStorage.getItem(storageName);
+        if (storedValueStr == null) return;
+
+        const localValue = JSON.parse(storedValueStr);
+        if (localValue !== get(store)) store.set(localValue);
+    });
+
+    return {
+        update:(...chats: Chat[])=>{
+            store.update((acc)=> [...acc, ...chats]);
+        },
+        // length of svelte store
+    l: () => get(store).length,  
+    } as const;
+}
